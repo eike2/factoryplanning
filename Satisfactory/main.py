4ea96aa1efd543339f8689fa96f5fe1898b05e91
eike2 <104501189+eike2@users.noreply.github.com>
2022-04-29 18:25:46 +0200

    update

--- Satisfactory/main.py
+++ Satisfactory/main.py
@@ -1,6 +1,8 @@
 import sys
 
+from PySide6.QtGui import QPalette, QColor
 from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import Qt
 
 from Satisfactory.database import createConnection
 from Satisfactory.gui import Window
@@ -10,6 +12,30 @@ def main():
     """Factoryplanning main function."""
     # Create the application
     app = QApplication(sys.argv)
+    # Force the style to be the same on all OSs:
+    app.setStyle("Fusion")
+
+    # Now use a palette to switch to dark colors:
+    dark_palette = QPalette()
+    dark_palette.setColor(QPalette.Window, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.WindowText, Qt.white)
+    dark_palette.setColor(QPalette.Base, QColor(35, 35, 35))
+    dark_palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.ToolTipBase, QColor(25, 25, 25))
+    dark_palette.setColor(QPalette.ToolTipText, Qt.white)
+    dark_palette.setColor(QPalette.Text, QColor(255, 255, 255))
+    dark_palette.setColor(QPalette.Button, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.ButtonText, Qt.white)
+    dark_palette.setColor(QPalette.BrightText, Qt.red)
+    dark_palette.setColor(QPalette.Link, QColor(42, 130, 218))
+    dark_palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+    dark_palette.setColor(QPalette.HighlightedText, QColor(35, 35, 35))
+    dark_palette.setColor(QPalette.Active, QPalette.Button, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.Disabled, QPalette.ButtonText, Qt.darkGray)
+    dark_palette.setColor(QPalette.Disabled, QPalette.WindowText, Qt.darkGray)
+    dark_palette.setColor(QPalette.Disabled, QPalette.Text, Qt.darkGray)
+    dark_palette.setColor(QPalette.Disabled, QPalette.Light, QColor(53, 53, 53))
+    QApplication.setPalette(dark_palette)
     # Connect to the database before creating any window
     if not createConnection(r"C:\Users\eikew\OneDrive\Dokumente\My Games\Satisfactory\factoryplanning\Satisfactory.db"):
         sys.exit(1)
